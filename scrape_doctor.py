7363277c65a3cd5e48cfde69fdefc6992ec7b254
Sevengek0 <giacomopav7@gmail.com>
2024-10-25 20:18:47 +0200

    Ricerca del nome cognome del medico e il numero di posti liberi con stampa del risultato finale

--- scrape_doctor.py
+++ scrape_doctor.py
@@ -5,7 +5,8 @@ import regex
 import requests
 from bs4 import BeautifulSoup
 
-url = "******"
+site_url = "https://salute.regione.veneto.it/servizi/cerca-medici-e-pediatri?p_p_id=MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&p_p_col_id=column-3&p_p_col_count=2&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_action=dettaglio&_MEDICI_WAR_portalgeoreferenziazione_INSTANCE_F5Pm_dmcreg="
+
 
 risposta = requests.get(url)
 
@@ -16,13 +17,22 @@ if risposta.status_code == 200:
     try:
         header = soup.table.tr.find_all('th')
         # text_head = header[0].text
-        reg_analisis_date = re.compile(r'\d\d/\d\d/\d\d\d\d')
+        body_tr = soup.table.tbody.find_all('tr')
+
+        body_posti = body_tr[0].find_all('td')
+
+        doct_name = soup.find(id='tab01').strong.text
+        reg_analysis_date = re.compile(r'\d\d/\d\d/\d\d\d\d')
         # reg_test = re.compile(r'[a-z]+')
-        data_analisi = reg_analisis_date.findall(header[0].text)
+        data_analisi = reg_analysis_date.findall(header[0].text)
         if data_analisi[0] == None:
             raise ValueError("data non trovata")
 
-        print("data ricerca: " + data_analisi[0])
+        finale = doct_name + " - " + data_analisi[0] + " - Posti liberi: " + body_posti[1].text
+
+        print(finale)
+
+        # print("data ricerca: " + data_analisi[0])
         # res_str = res.prettify()
         # print(text_head)
     except Exception as err:
