680dda59abd8a98f2e67506d6734b017091d6a87
Sevengek0 <giacomopav7@gmail.com>
2024-11-09 19:40:59 +0100

    Refactoring dei filtri di beautifulsoup; aggiunti tutti i parametri di ricerca medico (non funzionante la Distanza); Aggiunti commenti

--- getMeds.py
+++ getMeds.py
@@ -6,6 +6,7 @@ url = "https://salute.regione.veneto.it/servizi/cerca-medici-e-pediatri?p_p_id=M
 
 
 def estrai_luoghi(input):
+    # prendo la stringa di testo che contiene tutti i medici filtrati
     match = re.search(r"<luoghi>(.*?)</luoghi>", input, re.DOTALL)
     return match.group(1) if match else None
 
@@ -13,33 +14,29 @@ def estrai_luoghi(input):
 data = {
     "provincia": "VE",  #inserire il codice della provincia: BL PD RO TV VE VI VR
     "comune": "VENEZIA",        # inserire nome del comune: Venezia
+
     # Aggiungi altri parametri specificati nel form
+    "nome": "",
+    "cognome": "",
+    "indirizzoMedico": "",      # indirizzo del medico
+    "tipologia": "",         # medico medicina generale o pediatra: MMG o PLS
+    "indirizzo": "",            # indirizzo a cui si applica la distanza
+    #"distanza": "1",             # <1km o 1<x<5 o >5km : 1 o 2 o 3
+    #DISTANZA DA RIVEDERE perché non è questo il nome del parametro
+
 }
 
 # Effettuo la richiesta
 response = requests.post(url, data=data)
 
-
 if response.status_code == 200:
-    print(f"Risultato : \n")
-    print(response.text)  # HTML della pagina con risultati
+    output_html = BeautifulSoup(response.text, 'html.parser')
 
-    outputhtml = BeautifulSoup(response.text, 'html.parser')
-
-    with open("outputFiltroComune.html","w", encoding="utf-8") as o:
-        print(outputhtml, file=o)
-
-    onlyscript = outputhtml.div.find(class_='form-container')
-
-    jutext = onlyscript.decode_contents()
-
-    solo_luoghi = estrai_luoghi(jutext)
-    soupsoup =  BeautifulSoup(solo_luoghi, 'html.parser')
-
-    # res = soupsoup.find_all("luogo")
-    res = soupsoup.find_all("id")
-
-    result = [tag.get_text() for tag in res]
+    # filtro con beautiful soup
+    form_script = output_html.div.find(class_='form-container').decode_contents()
+    stringa_luoghi = estrai_luoghi(form_script)
+    luoghi = BeautifulSoup(stringa_luoghi, 'html.parser')
+    result = [tag.get_text() for tag in luoghi.find_all("id")]
 
     with open("scriptoutput.html","w", encoding="utf-8") as o:
         print(result, file=o)
